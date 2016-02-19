a897d7a4924e322d30feafa9222babef1e76e760
Qionghua Yang <yangqionghuasmailbox@126.com>
2016-02-19 17:00:23 +0800

    Anothe method.

--- tast_manager_cpu_wave/tast_manager_cpu_wave.cc
+++ tast_manager_cpu_wave/tast_manager_cpu_wave.cc
@@ -2,23 +2,58 @@
 #include <cmath>
 #include <iostream>
 
-void mySolution() {
+/* Method 0: Let for loop consume time f, Sleep consume time a(such as 5 ms);
+ * Given a CPU utilization cu, then cu = f / (f + a); For example f = a, cu = 50%. 
+ * When cu = (b + sin(c)) / d, b > 1, and d > b + 1 > 2, task manage plots sine
+ * CPU utilization curve; Then f = a * (b + sin(c)) / (d - b -sin(c)). This 
+ * function is a little bizarre, the value b and d should be tuned. */
+void mySolution0() {
   const float pi = 3.141592653;
-  const int N = 3000;
+  const int n = 3000;
   const int relative_frequency = 4;
 
-  double ft[N];      // Function table of one period.
-  for (int i=0; i<N; ++i) {
-    double radian = 2.0f * pi * i / N;
-    ft[i] = (3.5 + sin(radian)) / (4 - sin(radian));
+  double ft[n];      // Function table of one period.
+  for (int i=0; i<n; ++i) {
+    double radian = 2.0 * pi * i / n;
+    ft[i] = (3 + sin(radian)) / (3 - sin(radian));
     std::cout << ' ' << ft[i];
   }
 
   int i = 0;
   while (1) {
-    i %= N;
-    for (int j=0; j<8000000 * ft[i]; ++j) ;     // Intel T5870 @ 2GHz, 1.25 ns per loop.
-    Sleep(10);
+    i %= n;
+    for (int j=0; j<5000000 * ft[i]; ++j) ;     // About 1 ns per loop.
+    Sleep(5);
+    i += relative_frequency;
+  }
+}
+
+/* Method 1: The bizarre function in Method 0 is because of that we make Sleep 
+ * time constant. So let busy time conform 1 + sin(x) and idle time conform 
+ * 1 - sin(x), then cu = (1 + sin(x)) / 2. To avoid top and bottom fluctuation, 
+ * let f = 2 + six(x), a = 2 - six(x), then CPU utilization range will be [1/4, 3/4]. 
+ * But the curve tends to be discrete because of the shrinked range. Let f = A + sin(x), 
+ * a = B - six(x), range will be [(A-1)/(A+B), (A+1)/(A+B)]; When we want cu 
+ * range to be [10%, 100%], the A = 1.22, B = 1. */
+void mySolution1() {
+  const float pi = 3.141592653;
+  const int n = 3000;
+  const int relative_frequency = 130;
+  const int grain = 100;
+
+  double bt[n], it[n];    // Busy table, idle table.
+  for (int i=0; i<n; ++i) {
+    double radian = 2.0 * pi * i / n;
+    bt[i] = 1.22 + sin(radian);
+    it[i] = 1 - sin(radian);
+  }
+
+  int i = 0;
+  while (1) {
+    i %= n;
+    auto st = GetTickCount();   // Start time.
+    while (1000 * (GetTickCount() - st) < grain * 1000 * bt[i]) ;
+    Sleep(grain * it[i]);
     i += relative_frequency;
   }
 }
@@ -52,10 +87,13 @@ void beautyOfProgrammingSolution() {
 }
 
 int main(int argc, char * argv[]) {
-  int idx = 0;
+  int idx;    // Left uninitialized.
   if (argc != 1) idx = argv[1][0] - '0';
-  if (idx) beautyOfProgrammingSolution();
-  else mySolution();
+
+  if (idx == 0) mySolution0();
+  else if (idx == 1) mySolution1();
+  else beautyOfProgrammingSolution();
+
   return 0;
 }
 
